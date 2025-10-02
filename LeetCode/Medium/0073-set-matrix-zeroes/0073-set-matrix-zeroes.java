938dd91864565aa739edb5063e686edd9000877f
Ayush Jain <116939675+ayushcode12@users.noreply.github.com>
2025-10-02 12:55:33 +0530

    Time: 0 ms (100%), Space: 46 MB (16.77%) - LeetHub

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,41 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+
         int n = matrix.length;
         int m = matrix[0].length;
+        int[] row = new int[n];
+        int[] col = new int[m];
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
-                    markRow(matrix, i);
-                    markCol(matrix, j);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(matrix[i][j] == -11){
+                if(row[i] == 1 || col[j] == 1){
                    matrix[i][j] = 0;
                 }
             }
         }
 
-    }
-
-    private void markRow(int[][] matrix, int i){
-        for(int j = 0; j < matrix[0].length; j++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -11;
-            }
-        }
-    }
-
-    private void markCol(int[][] matrix, int j){
-        for(int i = 0; i < matrix.length; i++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -11;
-            }
-        }
     }
 
 }
