39ccb6f4a0179bddba325ba2d7591f18780134a6
Ayush Jain <116939675+ayushcode12@users.noreply.github.com>
2025-10-30 00:03:57 +0530

    Time: 0 ms (100%), Space: 42.2 MB (52.52%) - LeetHub

---
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean ans = false;
+
+        for(int i = 0; i < row; i++){
+            if(target <= matrix[i][col-1] && target >= matrix[i][0]){
+                ans = searchEachRow(matrix[i], col, target);
+                if(ans) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchEachRow(int[] matrix, int col, int target){
+        int low = 0;
+        int high = col - 1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(matrix[mid] == target) return true;
+
+            else if(matrix[mid] > target) high = mid - 1;
+
+            else low = mid + 1;
+        }
+        return false;
+    }
+}
