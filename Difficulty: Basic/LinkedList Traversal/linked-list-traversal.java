8346b7708a4c12ae1d60bfc5b2f197d6b78655b9
Ayush Jain <116939675+ayushcode12@users.noreply.github.com>
2025-11-13 00:54:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LinkedList Traversal/linked-list-traversal.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    public static void LinkedListTraversal(LinkedList<Integer> list) {
+        for(int val : list){
+            System.out.print(val + " ");
+        }
+    }
+}
