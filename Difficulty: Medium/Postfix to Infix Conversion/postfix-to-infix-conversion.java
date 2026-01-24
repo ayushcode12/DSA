43dfd65b42b8fe667625c20c5ddd18dd53c6ecff
Ayush Jain <116939675+ayushcode12@users.noreply.github.com>
2026-01-24 18:51:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Infix Conversion/postfix-to-infix-conversion.java
@@ -0,0 +1,29 @@
+// User function Template for Java
+
+class Solution {
+    static String postToInfix(String exp) {
+        
+        Stack<String> s = new Stack<>();
+        int n = exp.length();
+
+        // Traverse the postfix expression from left to right
+        for (int i = 0; i < n; i++) {
+            char c = exp.charAt(i);
+
+            // If the character is an operand, push it to the stack
+            if (Character.isLetterOrDigit(c)) {
+                s.push(String.valueOf(c));
+            } else {
+                // Pop two operands from the stack
+                String op2 = s.pop();
+                String op1 = s.pop();
+
+                // Form the new infix expression and push back to stack
+                s.push("(" + op1 + c + op2 + ")");
+            }
+        }
+
+        // The final element in the stack is the result
+        return s.peek();
+    }
+}
