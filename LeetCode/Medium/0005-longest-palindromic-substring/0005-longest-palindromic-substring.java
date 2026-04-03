4f176dcaacfede2caaacde662a26519bb1a6e708
Ayush Jain <116939675+ayushcode12@users.noreply.github.com>
2026-04-03 17:49:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
-        int n = s.length();
-        String ans = "";
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
+        int start = 0;
+        int end = 0;
 
-                int left = i;
-                int right = j;
-                boolean isPalindrome = true;
+        for(int i = 0; i < s.length(); i++){
 
-                // check if substring (i, j) is palindrome
-                while(left < right){
-                    if(s.charAt(left) != s.charAt(right)){
-                        isPalindrome = false;
-                        break;
-                    }
-                    left++;
-                    right--;
-                }
+            // Odd length
+            int len1 = expand(s, i, i);
 
-                // if palindrome, check length
-                if(isPalindrome){
-                    if(j - i + 1 > ans.length()){
-                        ans = s.substring(i, j + 1);
-                    }
-                }
+            // Even length
+            int len2 = expand(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            // update result
+            if(len > end - start){
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
 
-        return ans;
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right){
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return right - left - 1; // length of palindrome
     }
 }
